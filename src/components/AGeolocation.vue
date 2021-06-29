a3442eead5305f2cf36ba9986aa21b1b37956660
gormlabenz <gorm-labenz@hotmail.com>
2021-06-29 16:38:36 +0200

    added dot animation/ favicons

--- src/components/AGeolocation.vue
+++ src/components/AGeolocation.vue
@@ -1,8 +1,15 @@
 <template>
-  <transition name="fade">
+  <transition
+    enter-active-class="transition-all transition-fastest ease-out-quad"
+    leave-active-class="transition-all transition-faster ease-in-quad"
+    enter-class="opacity-0 scale-70"
+    enter-to-class="opacity-100 scale-100"
+    leave-class="opacity-100 scale-100"
+    leave-to-class="opacity-0 scale-70"
+  >
     <p v-if="!start && !loading">
       For the use we need
-      <mark class="user"> your geolocation</mark>.<br /><a
+      <mark class="user"> your location on earth</mark>.<br /><a
         v-if="!start"
         @click="get"
         class="
@@ -20,16 +27,35 @@
   </transition>
   <!--  -->
 
-  <transition name="fade">
+  <transition
+    enter-active-class="transition-all transition-fastest ease-out-quad"
+    leave-active-class="transition-all transition-faster ease-in-quad"
+    enter-class="opacity-0 scale-70"
+    enter-to-class="opacity-100 scale-100"
+    leave-class="opacity-100 scale-100"
+    leave-to-class="opacity-0 scale-70"
+  >
     <p v-if="loading">
       Exploring
       <mark class="text-signal">the solarsystem</mark>
-      and <mark class="user">your neighbourhood</mark>…
+      and <mark class="user">your neighbourhood</mark>
+      <span class="load-container ml-2" style="animation-delay: 0s">.</span>
+      <span class="load-container ml-2" style="animation-delay: 0.2s">.</span>
+      <span class="load-container ml-2" style="animation-delay: 0.4s">.</span>
+
+      Even at the speed of light, this can take over 10 seconds.
     </p>
   </transition>
 
   <!--  -->
-  <transition name="fade">
+  <transition
+    enter-active-class="transition-all transition-fastest ease-out-quad"
+    leave-active-class="transition-all transition-faster ease-in-quad"
+    enter-class="opacity-0 scale-70"
+    enter-to-class="opacity-100 scale-100"
+    leave-class="opacity-100 scale-100"
+    leave-to-class="opacity-0 scale-70"
+  >
     <p v-if="start && !loading">
       If <mark class="user">you</mark> were the
       <mark class="text-signal">sun</mark> with the diameter of
@@ -108,13 +134,42 @@ export default {
 mark {
   @apply bg-transparent text-stroke-0;
 }
+.load-container {
+  animation: load 2.5s ease-in-out infinite;
+  display: inline-block;
+}
+
+.load-container:nth-child(0) {
+  animation-delay: 0s;
+}
+.load-container:nth-child(1) {
+  animation-delay: 0.2s;
+}
+.load-container:nth-child(2) {
+  animation-delay: 0.4s;
+}
+
+@keyframes load {
+  0%,
+  100% {
+    transform: translateY(-20px);
+  }
+  50% {
+    transform: translateY(0);
+  }
+}
+
 .fade-enter-active,
 .fade-leave-active {
-  transition: all 0.3s;
+  transition: all 0.3s ease-in;
+}
+.fade-enter-to .fade-leave {
+  transform: translateY(100%);
+  opacity: 1;
 }
 .fade-enter,
 .fade-leave-to {
+  transform: translateY(-100%);
   opacity: 0;
-  margin-top: -10%;
 }
 </style>
