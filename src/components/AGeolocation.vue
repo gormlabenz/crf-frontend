29c1767a22e4a1358dde6f716c79c3453a87ca7d
gormlabenz <gorm-labenz@hotmail.com>
2021-06-30 16:18:53 +0200

    added tracking

--- src/components/AGeolocation.vue
+++ src/components/AGeolocation.vue
@@ -1,49 +1,59 @@
 <template>
-  <div>
-    <p v-if="!start && !loading">
-      For the use we need
-      <mark class="user"> your location on earth</mark>.<br /><a
-        v-if="!start"
-        @click="get"
-        class="
-          text-signal
-          hover:text-black
-          transition-all
-          text-stroke-0
-          cursor-pointer
-          underline
-        "
-      >
-        Okay
-      </a>
-    </p>
-    <p v-if="loading">
-      Exploring
-      <mark class="text-signal">the solarsystem</mark>
-      and <mark class="user">your neighbourhood</mark>
-      <span class="load-container ml-2" style="animation-delay: 0s">.</span>
-      <span class="load-container ml-2" style="animation-delay: 0.2s">.</span>
-      <span class="load-container ml-2" style="animation-delay: 0.4s">.</span>
-      Even at the speed of light, this can take over 10 seconds.
-    </p>
-    <p v-if="start && !loading">
-      If <mark class="user">you</mark> were the
-      <mark class="text-signal">sun</mark> with the diameter of
-      <mark class="user">{{ diameter + " m" }}</mark
-      >,
-      <span v-for="[name, ref] in Object.entries(referncesClean)" :key="name">
-        <mark class="text-signal">{{ name + " " }}</mark>
-        would be at
-        <mark
-          v-for="(tag, index) in Object.values(ref)"
-          :key="tag"
-          :class="'text-black-' + (500 - index * 200)"
-          class="capitalize"
-          >{{ " " + tag.replace("_", " ") + " " }}</mark
-        >.
-      </span>
-    </p>
-  </div>
+  <!-- Taiwlind needs to load classed, because text-black ist just loaded dynamicly -->
+  <p
+    class="
+      hidden
+      text-black-500
+      text-black-400
+      text-black-300
+      text-black-200
+      text-black-100
+      text-black-50
+    "
+  ></p>
+  <p v-if="!start && !loading">
+    For the use we need
+    <mark class="user"> your location on earth</mark>.<br /><a
+      v-if="!start"
+      @click="get"
+      class="
+        text-signal
+        hover:text-black
+        transition-all
+        text-stroke-0
+        cursor-pointer
+        underline
+      "
+    >
+      Okay
+    </a>
+  </p>
+  <p v-if="loading">
+    Exploring
+    <mark class="text-signal">the solarsystem</mark>
+    and <mark class="user">your neighbourhood</mark>
+    <span class="load-container ml-1" style="animation-delay: 0s">.</span>
+    <span class="load-container ml-1" style="animation-delay: 0.2s">.</span>
+    <span class="load-container ml-1" style="animation-delay: 0.4s">.</span>
+    Even at the speed of light, this can take over 10 seconds.
+  </p>
+  <p v-if="start && !loading">
+    If <mark class="user">you</mark> were the
+    <mark class="text-signal">sun</mark> with the diameter of
+    <mark class="user">{{ diameter + " m" }}</mark
+    >,
+    <span v-for="[name, ref] in Object.entries(referncesClean)" :key="name">
+      <mark class="text-signal">{{ name + " " }}</mark>
+      would be at
+      <mark
+        v-for="(tag, index) in Object.values(ref)"
+        :key="tag"
+        :class="'text-black-' + (500 - index * 200)"
+        class="capitalize"
+        >{{ " " + tag.replace("_", " ") + " " }}</mark
+      >.
+    </span>
+  </p>
 </template>
 
 <script>
@@ -101,7 +111,7 @@ export default {
 
 <style>
 p {
-  @apply leading-tight;
+  @apply leading-tight tracking-wide;
 }
 mark {
   @apply bg-transparent text-stroke-0;
